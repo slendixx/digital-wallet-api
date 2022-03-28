d4c57902e6393537bb3fcee1879a7adab008f9ea
Esteban <esteban321duran@gmail.com>
2022-03-28 13:33:12 -0300

    Refactored target hash duration into env variable
    Environment variable: BCRYPT_TARGET_HASH_DURATION_MS

---
+++ src/security/calculateIdealHashCost.js
@@ -0,0 +1,25 @@
+import bcrypt from 'bcrypt';
+/**
+ *  This function runs a micro-benchmark to figure out how fast the CPU is.
+ *  The target is for a bcrypt hash to take at least 250 ms.
+ */
+const TARGET_HASH_DURATION_MS =
+    process.env.BCRYPT_TARGET_HASH_DURATION_MS || 250;
+
+const calculateIdealHashCost = async () => {
+    let cost = 8;
+    const startTime = Date.now();
+    await bcrypt.hash('microbenchmark', cost);
+    const endTime = Date.now();
+    let hashDuration = endTime - startTime;
+    //Increasing cost by 1 would double the run time.
+    //Keep increasing cost until the estimated duration is over 250 ms
+    while (hashDuration <= TARGET_HASH_DURATION_MS) {
+        cost += 1;
+        hashDuration *= 2;
+    }
+
+    return cost;
+};
+
+export default calculateIdealHashCost;
