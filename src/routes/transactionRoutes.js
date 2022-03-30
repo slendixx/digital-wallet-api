b1d9b411f14a798a5cec0b1ab88d4f06e7f5fc47
Esteban <esteban321duran@gmail.com>
2022-03-30 13:57:46 -0300

    commented out unimplemented function calls
    Commented out unimplemented transaction controller function for
    debugging purporses.

--- src/routes/transactionRoutes.js
+++ src/routes/transactionRoutes.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const controller = require('../controllers/transactionController');
-const { getById } = require('../controllers/userController');
 //const passport = require('passport');
 //const { restrictTo } = require('../controllers/authController');
 
 const router = express.Router({ mergeParams: true });
-
+/*
 router.route('/').get(controller.getAll).post(controller.create);
 router
     .route('/:userId')
     .get(controller.getById)
     .patch(controller.updateById)
     .delete(controller.deleteById);
-
+*/
 module.exports = router;
