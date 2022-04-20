ae307a46a151a4756034baf4b8416853fb1dd550
Esteban <esteban321duran@gmail.com>
2022-04-20 01:09:35 -0300

    fixed bug when redirecting to home
    Added missing Navigate element to "/*" route for redirecting to
    /home when trying to access undefined routes.

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./views/home/Home";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
           <Route path="/account/transactions" />
           <Route path="/login" />
           <Route path="/signup" />
-          <Route path="*" />
+          <Route path="/*" element={<Navigate replace to="/home" />} />
         </Routes>
       </BrowserRouter>
     </div>
