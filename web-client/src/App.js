957f2453177cda318408b32e6da529b79fa504ac
Esteban <esteban321duran@gmail.com>
2022-04-22 19:08:17 -0300

    Added forms to router
    Added signup, forgot password & change password view to respective
    routes.

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -3,7 +3,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./views/home/Home";
 import Login from "./views/login/Login";
-
+import ForgotPassword from "./views/passwordRecovery/forgotPasswordForm/ForgotPasswordForm";
+import Signup from "./views/signup/Signup";
+import ChangePassword from "./views/passwordRecovery/changePasswordForm/ChangePasswordForm";
 function App() {
   return (
     <div className="App">
@@ -13,8 +15,10 @@ function App() {
           <Route path="/account" />
           <Route path="/account/transactions" />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" />
-          <Route path="/reset-password" />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/change-password" element={<ChangePassword />} />
+
           <Route path="/*" element={<Navigate replace to="/home" />} />
         </Routes>
       </BrowserRouter>
