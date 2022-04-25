3d5a506d3ea388b63106d2a15e0fbcce5462cbf3
Esteban <esteban321duran@gmail.com>
2022-04-25 19:30:43 -0300

    configured and extended functionality
    configured axios request so that they use same-site httponly cookies.
    modified sendRequest method optionally receive callbacks and instead return the request promise.

--- web-client/src/services/hooks/useRequestAPI.js
+++ web-client/src/services/hooks/useRequestAPI.js
@@ -1,21 +1,37 @@
 import axios from "axios";
-const APIHost = "http://127.0.0.1:3000/api/v1";
+const APIHost = "http://localhost:3000/api/v1";
 
 const useRequestAPI = () => {
   /**
-   * @description Perform an AJAX request to the App's API
+   * @description Perform an AJAX request to the App's API. If no resolve & reject handlers are provided, returns a promise to be handled
+   *
    * @param {Object} config
    */
   const sendRequest = ({
     method = "get",
     url = "",
     data = {},
-    headers = {},
+    headers = { crossDomain: true },
     withCredentials = true,
     resolveHandler,
     rejectHandler,
   }) => {
-    axios({ method, data, headers, url: APIHost + url })
+    if (!rejectHandler && !resolveHandler)
+      return axios({
+        method,
+        data,
+        headers,
+        url: APIHost + url,
+        withCredentials,
+      });
+
+    axios({
+      method,
+      data,
+      headers,
+      url: APIHost + url,
+      withCredentials,
+    })
       .then(resolveHandler)
       .catch(rejectHandler);
   };
